c462f8081e9fb6f027bf587f10fc5af0bb37e04d
Huy Nguyen <edwardhaythamconnor@gmail.com>
2023-12-12 10:25:00 +0700

    Update vid link

--- js/projectInfo.js
+++ js/projectInfo.js
@@ -76,7 +76,7 @@ const Projects = {
             SmallDescription: "Unity, C#, 2D Topdown Roguelike",
             LargeDescription: 'TBA',
             DownloadLink: "",
-            EmbedVideoLinkID: "https://youtu.be/Yrs19Ztgk7s",
+            EmbedVideoLinkID: "../vid/BigHands.mp4",
             NoOfProjectImages: 0,
             Category: "game"
         },
